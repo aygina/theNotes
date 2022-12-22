d93c3dd7099c7cd4eef97d1fe2ea6d82e3cd2fea
aygina <aygina.v@gmail.com>
2022-12-22 14:32:45 +0900

    done

--- index.js
+++ index.js
@@ -2,13 +2,29 @@ document.addEventListener('DOMContentLoaded', function (event) {
     let notes = localStorage.getItem('notes');
 
     if (notes != null) {
-        allNotes.innerHTML = notes;
+        let clearNotes = notes.replace('"', '')
+                                .replace('[', '')
+                                .replace(']', '')
+                                .replace('","', '\n')
+                                .replace('\n', ' ')
+                                .replace('\n', '')
+                                .replace('"', '')
+                                .replace(',"', '\n');
+        allNotes.innerHTML = clearNotes;
     }
 })
 
 arrNote = [];
+let saved = document.getElementById('cool');
 
-function saveNote() {
+let newNotes = document.getElementById("note");
+newNotes.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        sendNote();
+    }
+});
+
+function sendNote() {
     let allNotes = document.querySelector('#allNotes');
     let note = document.getElementById('note').value;
     let date = new Date();
@@ -20,24 +36,30 @@ function saveNote() {
         minute: '2-digit'
     });
 
-    let newNote = (`Заметка от ` + newDate + `:<br> ` + note + `<br>`);
+    let newNote = (`Заметка от ` + newDate + `: ` + note + `<br>`);
     arrNote.push(newNote);
 
     allNotes.innerHTML = arrNote.join('');
     document.getElementById('note').value = '';
+}
 
+
+function saveNote() {
     if (localStorage.getItem('notes') == null) {
         localStorage.setItem('notes', JSON.stringify(arrNote));
-
     } else {
         let previousNotes = localStorage.getItem('notes');
         let updatedNotes = JSON.parse(previousNotes);
         updatedNotes.push(arrNote);
         localStorage.setItem('notes', JSON.stringify(updatedNotes));
     }
+
+    saved.innerHTML = 'Сохранено!';
 }
 
-    function clearStorage() {
-        localStorage.clear();
-        allNotes.innerHTML = '';
-    }
+function clearStorage() {
+    localStorage.clear();
+    allNotes.innerHTML = '';
+    arrNote = [];
+    saved.innerHTML = '';
+}
